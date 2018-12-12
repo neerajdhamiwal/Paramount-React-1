dcba45e75ebd7e6b971fd1ad6de7ad68541fbb0f
Priya <priya.k@opensenselabs.com>
2018-12-12 14:32:01 +0530

    AR-00: Our certificate slider.

--- src/component/AwardBanner.jsx
+++ src/component/AwardBanner.jsx
@@ -17,9 +17,6 @@ class AwardBanner extends React.Component{
           <div className="medium-5 cell small-order-change">
             <h3 className="banner-info"><span>{ReactHtmlParser(this.props.nodeData[0].node_title)}</span><br/>
                 {ReactHtmlParser(this.props.nodeData[0].node_subtitle_title)}</h3>
-            {/*<h6>Lorem Ipsum is simply dummy text of the printing and typesetting indu*/}
-              {/*stry. Lorem Ipsum has been the industry's standard dummy text ever since the */}
-              {/*1500s, when an unknown printer took a galley of type.</h6>*/}
             <p>{ReactHtmlParser(this.props.nodeData[0].node_description)}</p>
             <p>{ReactHtmlParser(this.props.nodeData[0].node_body)}</p>
           </div>
